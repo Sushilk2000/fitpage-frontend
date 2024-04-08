b0bad595e1cc14abff921386b060558fa9fc55a4
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-04-08 11:07:44 +0530

    few changes

--- src/Components/StockList.jsx
+++ src/Components/StockList.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
-import { useStocksContext } from "../context/context";
 import { useEffect, useState } from "react";
 function StockList() {
-  const [stockItems, setStockItems] = useState([]);
+  const [stocks, setStocks] = useState([]);
   useEffect(() => {
     const getStocks = async () => {
       try {
-        if (stockItems.length) return setStockItems(stockItems);
+        if (stocks.length) return setStocks(stockItems);
 
         const response = await fetch(
           "https://fitpage-backend-wegt.onrender.com/api/stocks/"
         );
         if (response.status === 200) {
           const data = await response.json();
-          setStockItems(data);
+          setStocks(data);
         }
       } catch (error) {
-        console.log("ERROR in getStocks", error);
+        console.log("Something went wrong: ", error);
       }
     };
     getStocks();
@@ -24,7 +23,7 @@ function StockList() {
   return (
     <div className="w-screen h-screen flex justify-center py-32">
       <div className="rounded-2xl shadow w-max h-max border py-4 border-white flex flex-col gap-4 px-8 shadow-white hover:scale-105 ease-in-out duration-300">
-        {stockItems.map((stock) => (
+        {stocks.map((stock) => (
           <Link to={`/${stock.id}`}>
             <div className="cursor-pointer hover:underline hover:border-b pb-2">
               <p className="text-xl">{stock.name}</p>
--- src/Components/StockVariableIndicator.jsx
+++ src/Components/StockVariableIndicator.jsx
@@ -3,17 +3,13 @@ import { useState } from "react";
 const StockVariableIndicator = ({ variable }) => {
   const [parameter, setParameter] = useState(variable.default_value);
 
-  const getUpperCase = (text) => {
-    return text && text.toUpperCase();
-  };
-
   return (
     <>
       <div className="w-screen h-screen flex justify-center py-32">
         {variable && (
           <div className="border border-white min-h-64 h-max min-w-[30%] w-max flex gap-4 flex-col px-4 py-4 rounded-xl hover:scale-105 duration-300 ease-in-out">
             <div>
-              <div>{getUpperCase(variable.study_type)}</div>
+              <div>{variable.study_type}</div>
               <div className="stock-list__title">Set Parameters</div>
             </div>
             <div className="bg-white text-black h-20 py-2 px-2">
